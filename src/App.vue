ca12c86a6cd9931ee0a37e0c48c7471afaa004ac
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-11-18 21:23:27 +0700

    refactor App.vue to comment out unused components and integrate CardPokemon

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,46 @@
 <script setup lang="ts">
-import { ref } from "vue";
+// import { ref } from "vue";
 // import Counter from "./components/Counter.vue";
 // import ConditionalRendering from "./components/ConditionalRendering.vue";
 
 // Menyimpan data user didalam object
-let dataUsers = ref([
-  {
-    name: "Nabil",
-    age: 22,
-  },
-  {
-    name: "Fajar",
-    age: 32,
-  },
-  {
-    name: "Adit",
-    age: 15,
-  },
-]);
+// let dataUsers = ref([
+//   {
+//     name: "Nabil",
+//     age: 22,
+//   },
+//   {
+//     name: "Fajar",
+//     age: 32,
+//   },
+//   {
+//     name: "Adit",
+//     age: 15,
+//   },
+// ]);
 
-function detailUser(name: string, age: number) {
-  alert(`Nama: ${name} | Usia: ${age}`);
-}
+// function detailUser(name: string, age: number) {
+//   alert(`Nama: ${name} | Usia: ${age}`);
+// }
 
-import Card from "./components/Card.vue";
+// import Card from "./components/Card.vue";
+
+import CardPokemon from "./components/CardPokemon.vue";
 </script>
 
 <template>
   <!-- <Counter /> -->
   <!-- <ConditionalRendering /> -->
 
-  <Card
+  <!-- <Card
     v-for="data in dataUsers"
     :key="data.name"
     :name="data.name"
     :age="data.age"
     :action="detailUser"
-  />
+  /> -->
+
+  <CardPokemon />
 </template>
 
 <style scoped></style>
