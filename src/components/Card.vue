3e71224c79a9b5227f1cf66d4bc09897a074c44c
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-11-18 20:54:59 +0700

    create web API Vue js

---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface Users {
+  name: string;
+  age: number;
+  action: Function;
+}
+
+export default defineComponent<Users>({
+  props: {
+    name: String,
+    age: Number,
+    action: Function,
+  },
+});
+</script>
+
+<template>
+  <section>
+    <div class="card">
+      <h1>Nama: {{ $props.name }}</h1>
+      <h2>Usia: {{ $props.age }}</h2>
+      <button @click="$props.action($props.name, $props.age)">
+        Detail user
+      </button>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.card {
+  border: 2px solid white;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref<number>(0);
+
+const increment = (): void => {
+  count.value++;
+};
+
+const decrement = (): void => {
+  if (count.value > 0) {
+    count.value--;
+  }
+};
+</script>
+
+<template>
+  <h1>Hello Vue JS</h1>
+  <p>Count: {{ count }}</p>
+  <button @click="increment">Tambah</button>
+  <button @click="decrement">Kurang</button>
+</template>
+
+<style scoped></style>
