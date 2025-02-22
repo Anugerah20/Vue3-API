a8e59962ba8155b1235a0b302b5e9ea599db4151
Nabil Anugerah <nabilpangestu123@gmail.com>
2025-02-22 17:01:14 +0700

    Update: enhance layout and styling of DetailPokemon component for better readability

--- src/components/DetailPokemon.vue
+++ src/components/DetailPokemon.vue
@@ -44,43 +44,52 @@ onMounted(() => {
 });
 </script>
 <template>
-  <section v-if="pokemonDetail" class="max-w-4xl mx-auto p-2 mt-10 border-2">
-    <a href="/" class="underline focus:font-bold" @click="router.back()"
-      >Back to Home</a
+  <div class="flex flex-col justify-center items-center mx-5">
+    <section
+      v-if="pokemonDetail"
+      class="max-w-4xl p-10 mt-10 border-2 rounded-md"
     >
-    <div class="text-center">
-      <img
-        :src="pokemonDetail.sprites.front_default"
-        :alt="pokemonDetail.name"
-        class="w-40 h-40 mx-auto"
-      />
-      <h1 class="text-2xl font-bold mb-5">{{ pokemonDetail.name }}</h1>
-    </div>
-    <div class="flex flex-wrap flex-col justify-center">
-      <div class="flex justify-center items-center gap-x-5 my-2">
-        <p><strong>Height:</strong> {{ pokemonDetail.height }}</p>
-        <p><strong>Weight:</strong> {{ pokemonDetail.weight }}</p>
+      <a href="/" class="underline focus:font-bold" @click="router.back()"
+        >Back to Home</a
+      >
+      <div class="text-center">
+        <img
+          :src="pokemonDetail.sprites.front_default"
+          :alt="pokemonDetail.name"
+          class="w-40 h-40 mx-auto"
+        />
+        <h1 class="text-2xl font-bold mb-5">{{ pokemonDetail.name }}</h1>
       </div>
-      <div class="flex justify-center items-center gap-x-5 my-2">
-        <p><strong>Types:</strong></p>
-        <ul class="flex flex-col">
-          <li v-for="type in pokemonDetail.types" :key="type.type.name">
-            {{ type.type.name }}
-          </li>
-        </ul>
-        <p><strong>Abilities:</strong></p>
-        <ul class="flex flex-col">
-          <li
-            v-for="ability in pokemonDetail.abilities"
-            :key="ability.ability.name"
-          >
-            {{ ability.ability.name }}
-          </li>
-        </ul>
+      <div class="flex flex-wrap flex-col justify-center">
+        <div class="flex justify-center items-center gap-4 my-2">
+          <p class="font-bold">Height :</p>
+          <span>{{ pokemonDetail.height }}</span>
+          <p class="font-bold">Weight :</p>
+          <span>{{ pokemonDetail.weight }}</span>
+        </div>
+        <div class="flex justify-between flex-wrap">
+          <p class="font-bold flex flex-col">Types :</p>
+          <p class="font-bold flex">Abilities :</p>
+        </div>
+        <div class="flex justify-between my-2">
+          <ul class="flex flex-col">
+            <li v-for="type in pokemonDetail.types" :key="type.type.name">
+              {{ type.type.name }}
+            </li>
+          </ul>
+          <ul class="flex flex-wrap flex-col">
+            <li
+              v-for="ability in pokemonDetail.abilities"
+              :key="ability.ability.name"
+            >
+              {{ ability.ability.name }}
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </section>
-  <section v-else class="flex justify-center items-center min-h-screen">
-    <p class="text-center animate-bounce">Loading...</p>
-  </section>
+    </section>
+    <section v-else class="flex justify-center items-center min-h-screen">
+      <p class="text-center animate-bounce">Loading...</p>
+    </section>
+  </div>
 </template>
