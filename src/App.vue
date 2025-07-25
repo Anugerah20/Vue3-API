4b35ee1e71612d3699ccf5a685b235406922cd24
Nabil Anugerah Pangestu <nabilpangestu123@gmail.com>
2025-07-25 16:12:06 +0700

    Update: implement dark mode initialization in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useDarkModeStore } from "./stores/darkModeStore";
+
+const darkModeStore = useDarkModeStore();
+
+onMounted(() => {
+  darkModeStore.initialize();
+});
+</script>
 
 <template>
   <router-view></router-view>
